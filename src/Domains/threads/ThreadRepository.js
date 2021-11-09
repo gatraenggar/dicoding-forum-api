fef0cad24f4902ca4f2c8a11173aeaf7cf071539
gatraenggar <gatraenggarwicaksono@gmail.com>
2021-11-10 02:02:09 +0800

    Add the missing verifyAvailableThread test & behaviour

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,13 +1,15 @@
-/* eslint-disable lines-between-class-members */
-/* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
-/* eslint-disable space-before-blocks */
-
+/* eslint-disable class-methods-use-this */
 class ThreadRepository {
-  async addThread(thread){
+  async addThread(thread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
-  async getThreadById(threadId){
+
+  async verifyAvailableThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThreadById(threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
